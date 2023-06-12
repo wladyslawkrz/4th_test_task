135ad5b486a42a923131855df08b4594c6f05d84
wladyslawkrz <v.kvetkovsky@yandex.by>
2023-06-12 13:51:31 +0300

    Implemented users/me route in the users.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/decorators';
+import { User } from 'src/entities';
+import { JwtGuard } from 'src/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+}
