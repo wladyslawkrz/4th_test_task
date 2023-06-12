8710686792185e1b214f6dfc6b8da89929d20ed9
wladyslawkrz <v.kvetkovsky@yandex.by>
2023-06-12 10:08:46 +0300

    Implemented auth system controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthDto } from './dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post('signup')
+  signUp(@Body() dto: AuthDto) {
+    return this.authService.signUp(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signIn(@Body() dto: AuthDto) {
+    return this.authService.signUp(dto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import * as models from '../entities';
 
-@Injectable()
-export class AuthService {}
+@Injectable({})
+export class AuthService {
+  async signUp(dto: AuthDto) {
+    const passwordHashed = await argon.hash(dto.password);
+
+    return 'registered';
+  }
+  async signIn(dto: AuthDto) {
+    const user = await models.User.findOne({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    return user.toJSON();
+  }
+}
