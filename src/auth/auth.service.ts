37c9163e54dbc5fbf01ff2b0eed457f898e47cd6
wladyslawkrz <v.kvetkovsky@yandex.by>
2023-06-12 11:45:17 +0300

    Implemented signup function and enabled global ValidationPipe

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import * as models from '../entities';
+import { Role } from 'src/entities/enum';
+import { UniqueConstraintError } from 'sequelize';
 
 @Injectable({})
 export class AuthService {
   async signUp(dto: AuthDto) {
     const passwordHashed = await argon.hash(dto.password);
 
-    return 'registered';
+    try {
+      await models.User.create({
+        email: dto.email,
+        passwordHashed: passwordHashed,
+        userRole: Role.regularUser,
+      });
+    } catch (error) {
+      if (error instanceof UniqueConstraintError) {
+        throw new HttpException('Email is already in use', HttpStatus.CONFLICT);
+      }
+    }
   }
+
   async signIn(dto: AuthDto) {
     const user = await models.User.findOne({
       where: {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 5000;
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   await app.listen(PORT, () => {
     console.log(`Server is started on ${PORT} port`);
