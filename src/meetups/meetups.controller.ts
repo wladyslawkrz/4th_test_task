12e1009c8f118d38308cece93337ac4c3785bbc0
wladyslawkrz <v.kvetkovsky@yandex.by>
2023-06-13 13:19:29 +0300

    Written base endpoint for meetups entity

--- src/meetups/meetups.controller.ts
+++ src/meetups/meetups.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('meetups')
-export class MeetupsController {}
+export class MeetupsController {
+  @Get()
+  getAllMeetups() {
+    return 'all of the meetups r here';
+  }
+
+  @Get(':id')
+  getMeetupById(@Param() params: any) {
+    return `this is ${params.id} meetup`;
+  }
+
+  @Post('create')
+  createMeetup() {
+    return 'meetup has been created';
+  }
+
+  @Put('update/:id')
+  updateMeetupInfo() {
+    return 'meetup info has been updated';
+  }
+
+  @Delete('delete/:id')
+  deleteMeetup() {
+    return 'meetup has been deleted';
+  }
+
+  @Get()
+  getMeetups(
+    @Query('search') search: string,
+    @Query('filter') filter: string,
+    @Query('sort') sort: string,
+  ) {
+    return 'search/filter/sort';
+  }
+}
