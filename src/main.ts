c5d6d73dd3353beaaf074d0498fbc4a15409ebc3
wladyslawkrz <v.kvetkovsky@yandex.by>
2023-06-18 13:18:19 +0300

    fix: deleted unnecessary files and imported the cookie-parser module into our app

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 8080;
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  app.use(cookieParser());
 
   await app.listen(PORT, () => {
     console.log(`Server is started on ${PORT} port`);
